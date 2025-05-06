99ac9ebc30419d5ba94459276d7e75dea9f2fb75
Mohsen Nayebi <122830808+mohsen-nyb@users.noreply.github.com>
2025-05-06 15:07:05 -0500

    Update train_mae.py

--- train_mae.py
+++ train_mae.py
@@ -13,20 +13,6 @@ import numpy as np
 import mlflow
 from models import ECG_CNN_Encoder, ECG_Classifier, ECG_Autoencoder
 from utils import *
-
-
-
-#train
-
-import os
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import numpy as np
-import pandas as pd
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-import matplotlib.pyplot as plt
-from tqdm import tqdm
 from torch.utils.data import DataLoader, random_split
 
 
